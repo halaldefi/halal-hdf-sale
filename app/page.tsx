5928e8aa64f96274baf25d1f7acf59c37abec66b
Ahmed Hamza <hdamnit@gmail.com>
2024-10-29 20:39:07 +0530

    Refactor page layout and update styles in page.tsx and SaleCardSection.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,9 +29,9 @@ export default function Home() {
                   {/* Audited Button */}
 
                 </div>
-                <div className="absolute right-4 bottom-4 flex items-center gap-1 px-2 py-1 w-fit bg-[#dbf4f0] rounded-xl">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="text-[#1ebaa5]" width="16" height="16" viewBox="0 0 16 16"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M14.25 8.75c-.5 2.5-2.385 4.854-5.03 5.38A6.25 6.25 0 0 1 3.373 3.798C5.187 1.8 8.25 1.25 10.75 2.25" /><path d="m5.75 7.75l2.5 2.5l6-6.5" /></g></svg>
-                  <span className="text-[#1ebaa5] text-xs font-semibold">Audited</span>
+                <div className="absolute right-4 bottom-4 flex items-center gap-1 px-2.5 py-1 w-fit bg-[#dbf4f0] rounded-full">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="text-[#1ebaa5]" width="20" height="20" viewBox="0 0 16 16"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M14.25 8.75c-.5 2.5-2.385 4.854-5.03 5.38A6.25 6.25 0 0 1 3.373 3.798C5.187 1.8 8.25 1.25 10.75 2.25" /><path d="m5.75 7.75l2.5 2.5l6-6.5" /></g></svg>
+                  <span className="text-[#1ebaa5] font-semibold">Audited</span>
                 </div>
               </div>
             </div>
