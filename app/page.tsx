85092231bec4cf9fb93d7ed1852bec0b319742f1
Ahmed Hamza <hdamnit@gmail.com>
2024-10-29 19:32:31 +0530

    Refactor page layout and add resizable panels

--- app/page.tsx
+++ app/page.tsx
@@ -9,26 +9,28 @@ import {
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen overflow-hidden font-[family-name:var(--font-urbanist)]">
-      <header>
-        <h1 className="text-3xl font-semibold p-6">Resizable Panels</h1>
+    <main className="flex flex-col h-screen w-screen overflow-hidden font-[family-name:var(--font-urbanist)]">
+      <header className="flex-none">
+        <h1 className="text-3xl font-semibold p-4 sm:p-6">Halal Defi Header</h1>
       </header>
-      <ResizablePanelGroup
-        direction="horizontal"
-        className="h-full w-full rounded-lg border"
-      >
-        <ResizablePanel defaultSize={66.66}>
-          <div className="flex h-full items-center justify-center p-6">
-            <span className="font-semibold">One</span>
-          </div>
-        </ResizablePanel>
-        <ResizableHandle />
-        <ResizablePanel defaultSize={33.33}>
-          <div className="flex h-full items-center justify-center p-6">
-            <AccordionSection />
-          </div>
-        </ResizablePanel>
-      </ResizablePanelGroup>
+      <div className="flex-1 min-h-0"> {/* Added padding */}
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="h-full rounded-lg border"
+        >
+          <ResizablePanel defaultSize={66.66}>
+            <div className="flex flex-col h-full p-3 sm:p-6">
+              <div className="flex-1 min-h-0 border-solid border-2 border-orange-500 shadow-custom overflow-auto"></div>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel defaultSize={33.33}>
+            <div className="flex justify-center items-center h-full p-3 sm:p-6 overflow-auto">
+              <AccordionSection />
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
     </main>
   );
 }
