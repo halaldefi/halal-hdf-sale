eca85d44dd41be2bd21421a49e6771b0385a2245
Ahmed Hamza <hdamnit@gmail.com>
2024-11-01 18:23:15 +0530

    Enhance AccordionSection with improved text styling for better readability and embed instructional video

--- components/features/faq/AccordionSection.tsx
+++ components/features/faq/AccordionSection.tsx
@@ -14,7 +14,7 @@ export function AccordionSection() {
       >
         <AccordionTrigger>How to Buy</AccordionTrigger>
         <AccordionContent>
-          <p>
+          <p className="text-[16px] pb-1">
             You can buy by following the steps below:
           </p>
           <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0 }}><iframe src="https://www.loom.com/embed/f49f5be56d2d4b4a84f8792996ccec50?sid=3a2a6f5b-4cbf-4b53-820e-52596ba83603&hideEmbedTopBar=true" frameBorder="0" allowFullScreen style={{ position: 'absolute', top: 0, left: 0, width: "100%", height: "100%"}}></iframe></div>
@@ -26,8 +26,10 @@ export function AccordionSection() {
       >
         <AccordionTrigger>Project Details</AccordionTrigger>
         <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
+          <p className="text-[16px]">
+            Yes. It comes with default styles that matches the other
+            components&apos; aesthetic.
+          </p>
         </AccordionContent>
       </AccordionItem>
       <AccordionItem 
@@ -36,7 +38,7 @@ export function AccordionSection() {
       >
         <AccordionTrigger>Token Pricing</AccordionTrigger>
         <AccordionContent>
-          Price of the Token will increase by 4 per million $HDF (single batch) sold. and other details will be here.
+          <p className="text-[16px]">Price of the Token will increase by 4 per million $HDF (single batch) sold. and other details will be here.</p>
         </AccordionContent>
       </AccordionItem>
       <AccordionItem 
@@ -45,8 +47,10 @@ export function AccordionSection() {
       >
         <AccordionTrigger>Other Info</AccordionTrigger>
         <AccordionContent>
-          Yes. It&apos;s animated by default, but you can disable it if you
-          prefer.
+          <p className="text-[16px]">
+            Yes. It&apos;s animated by default, but you can disable it if you
+            prefer.
+          </p>
         </AccordionContent>
       </AccordionItem>
     </Accordion>
