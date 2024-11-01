1505ec5344c9e6a3b398100180d1b74b3c726322
Ahmed Hamza <hdamnit@gmail.com>
2024-11-01 18:35:24 +0530

    Enhance AccordionSection with improved trigger styling and responsive video embedding for better user experience

--- components/features/faq/AccordionSection.tsx
+++ components/features/faq/AccordionSection.tsx
@@ -12,19 +12,33 @@ export function AccordionSection() {
         value="item-1" 
         className="transition-colors duration-200 hover:bg-[#F8F6F2] px-4 data-[state=open]:bg-[#f8f7f4] data-[state=open]:rounded-md"
       >
-        <AccordionTrigger>How to Buy</AccordionTrigger>
+        <AccordionTrigger className="hover:no-underline">How to Buy</AccordionTrigger>
         <AccordionContent>
           <p className="text-[16px] pb-1">
             You can buy by following the steps below:
           </p>
-          <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0 }}><iframe src="https://www.loom.com/embed/f49f5be56d2d4b4a84f8792996ccec50?sid=3a2a6f5b-4cbf-4b53-820e-52596ba83603&hideEmbedTopBar=true" frameBorder="0" allowFullScreen style={{ position: 'absolute', top: 0, left: 0, width: "100%", height: "100%"}}></iframe></div>
+          <div 
+            className="relative w-full"
+            style={{ 
+              paddingBottom: '56.25%',
+              display: 'block',
+            }}
+          >
+            <iframe 
+              src="https://www.loom.com/embed/f49f5be56d2d4b4a84f8792996ccec50?sid=3a2a6f5b-4cbf-4b53-820e-52596ba83603&hideEmbedTopBar=true" 
+              frameBorder="0" 
+              allowFullScreen 
+              loading="eager"
+              className="absolute top-0 left-0 w-full h-full"
+            />
+          </div>
         </AccordionContent>
       </AccordionItem>
       <AccordionItem 
         value="item-2" 
         className="transition-colors duration-200 hover:bg-[#F8F6F2] px-4 data-[state=open]:bg-[#f8f7f4] data-[state=open]:rounded-md"
       >
-        <AccordionTrigger>Project Details</AccordionTrigger>
+        <AccordionTrigger className="hover:no-underline">Project Details</AccordionTrigger>
         <AccordionContent>
           <p className="text-[16px]">
             Yes. It comes with default styles that matches the other
@@ -36,7 +50,7 @@ export function AccordionSection() {
         value="item-3" 
         className="transition-colors duration-200 hover:bg-[#F8F6F2] px-4 data-[state=open]:bg-[#f8f7f4] data-[state=open]:rounded-md"
       >
-        <AccordionTrigger>Token Pricing</AccordionTrigger>
+        <AccordionTrigger className="hover:no-underline">Token Pricing</AccordionTrigger>
         <AccordionContent>
           <p className="text-[16px]">Price of the Token will increase by 4 per million $HDF (single batch) sold. and other details will be here.</p>
         </AccordionContent>
@@ -45,7 +59,7 @@ export function AccordionSection() {
         value="item-4" 
         className="transition-colors duration-200 hover:bg-[#F8F6F2] px-4 data-[state=open]:bg-[#f8f7f4] data-[state=open]:rounded-md"
       >
-        <AccordionTrigger>Other Info</AccordionTrigger>
+        <AccordionTrigger className="hover:no-underline">Other Info</AccordionTrigger>
         <AccordionContent>
           <p className="text-[16px]">
             Yes. It&apos;s animated by default, but you can disable it if you
